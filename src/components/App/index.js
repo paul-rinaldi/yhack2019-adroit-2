c0c042d3038d0e37577d6416681337b8e5488bd7
Paul Rinaldi <paulrinaldi10@yahoo.com>
2019-10-26 10:12:02 -0400

    Mods Searchbar, imports working

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import * as ROUTES from '../../constants/routes';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import SearchPage from '../SearchBar';
 
 const App = () => (
-  <p>Hello world</p>
+  <Router>
+    <Route exact path={ROUTES.SEARCH} component={SearchPage} />
+  </Router>
 );
 
 export default App;
--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,21 +1,14 @@
-import react from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
 
-const useStyles = makeStyles(theme => ({
-    root: {
-        padding: theme.spacing(3, 2),
-    },
-}));
 
 const SearchPage = () => (
     <div>
-      <Paper className={classes.root}>
-        <Typography button>
-          <h2>I would like to know about</h2> 
-        </Typography>
-      </Paper>
+        <TextField>
+            I would like to know about...
+        </TextField>
     </div>
 );
 
