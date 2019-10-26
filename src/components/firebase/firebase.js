966d67bda49261cf0fa132ca183684821e42171b
Hung Nguyen <viethung1996@gmail.com>
2019-10-26 10:11:46 -0400

    add material-ui

---
+++ src/components/firebase/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDEJG6QhLCUUWus0C0NR-4kE4T9puiDGl0',
+  authDomain: 'yhack-adroit.firebaseapp.com',
+  databaseURL: 'https://yhack-adroit.firebaseio.com',
+  projectId: 'yhack-adroit',
+  storageBucket: 'yhack-adroit.appspot.com',
+  messagingSenderId: '318699708866',
+  appId: '1:318699708866:web:fc7917a3a815b87feaf65b',
+  measurementId: 'G-HJG2D7S15W'
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const firestore = firebase.firestore();
+export default firebase;
