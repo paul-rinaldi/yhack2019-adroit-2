4e1cbf8b7d32552b5f03a04af7828eef69119ff1
Paul Rinaldi <paulrinaldi10@yahoo.com>
2019-10-26 11:21:10 -0400

    Adds styled search bar, need to add firebase calls and methods

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,15 +1,45 @@
 import React from 'react';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
+import style from './searchStyle.css';
 
-
-const SearchPage = () => (
-    <div>
-        <TextField>
-            I would like to know about...
-        </TextField>
-    </div>
-);
+const SearchPage = () => {
+    const [values, setValues] = React.useState({
+        search: ''
+    });
+    const handleChange = name => event => {
+        setValues({ ...values, [name]: event.target.value });
+    };
+    console.log(values);
+    return (
+        <div>
+            <div id="cover">
+            <form onSubmit={() => handleChange('search')}>
+                <div class="tb">
+                <div class="td">
+                    <input type="text" placeholder="Jetblue" required />
+                </div>
+                <div class="td" id="s-cover">
+                    <button type="submit">
+                    <div id="s-circle"></div>
+                    <span></span>
+                    </button>
+                </div>
+                </div>
+            </form>
+            </div>
+            <form>
+            <TextField 
+                id="filled-search"
+                label="Find negative sentiments about... "
+                value={values.search}
+                onChange={handleChange('search')}
+                margin="normal"
+                variant="filled"
+                style={style}
+            />
+            </form>
+        </div>
+    ); 
+};
 
 export default SearchPage;
