be140c5839544930fc55deada0263cda81840323
Hung Nguyen <viethung1996@gmail.com>
2019-10-26 12:05:34 -0400

    fixe site structure

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './App.css';
-import ResultPage from './pages/result/result.component';
-
-const App = () => {
-  return <ResultPage />;
-};
-
-export default App;
